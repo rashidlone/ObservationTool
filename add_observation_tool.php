875ba2d2c228636259b0882fe8fb735152db1465
Zumer Jan <zumer.fws@gmail.com>
2025-05-16 11:40:21 +0530

    Update add_observation_tool.php

--- add_observation_tool.php
+++ add_observation_tool.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>NL Observation Software - Foundation World School</title>
+  <title>Observation Software - Foundation World School</title>
 
   <?php include 'css.php'; ?>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
